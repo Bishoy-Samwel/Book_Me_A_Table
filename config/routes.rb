ab51791b9456330c76c1085a4d623b6c3b0f187b
Bishoy <bishoy.samwuel@gmail.com>
2022-01-14 15:51:07 +0200

    Add routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,12 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'randomMessage', to: 'messages#random_message'
       resources :restaurants, only: [:index, :show, :create, :destroy]
-      resources :users, param: :username, only: [:create] do
+      resources :users, only: [:create] do
         resources :reservations, only: [:index, :create, :destroy]
       end
       resources :sessions, only: [:create]
+      get 'logged_in', to: 'sessions#logged_in'
+      delete 'logout', to: 'sessions#logout'
     end
   end
 end
