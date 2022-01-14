af82bdd682acbb55ce99f840575913b63fce43b4
Bishoy <bishoy.samwuel@gmail.com>
2022-01-14 15:23:34 +0200

    Add index, show actions

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,7 +1,15 @@
 module Api
   module V1
     class RestaurantsController < ApplicationController
-      
+      def index
+        @restaurants = Restaurant.all
+        render json: @restaurants
+      end
+      def show
+        item_id = params[:id]
+        @restaurant = Restaurant.find(item_id)
+        render json: @restaurant
+      end
     end
   end
 end
