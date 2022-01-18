7e9abb296bdee9ace519cbbad9ba1e1f8836cf12
luqmanmusah <musahluqman@gmail.com>
2022-01-18 09:52:22 +0000

    Fix imports

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -15,29 +15,31 @@ import Restaurants from './restaurants.jsx';
 import './navbar.css';
 import './App.css';
 import ElementDetails from './ElementDetails/Details';
+import Elements from './Elements/Elements';
+import { store } from './redux/configureStore';
 const storeConfig = store();
 
 export default function App() {
-   //menuCollapse state using useState hook
+   // menuCollapse state using useState hook
    const [menuCollapse, setMenuCollapse] = useState(false)
-   //custom function that will change menucollapse state from false to true and true to false
+   // custom function that will change menucollapse state from false to true and true to false
    const menuIconClick = () => {
-       //condition checking to change state from true to false and vice versa
+       // condition checking to change state from true to false and vice versa
        menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
    };
   return (
     <Provider store={storeConfig}>
       <Router>
+          
           <Navbar menuCollapse={menuCollapse} menuIconClick={menuIconClick} />
-          <div className={menuCollapse ? 'ml-2' : 'ml-5'} /> 
+          <div className={menuCollapse ? 'ml-2' : 'ml-5'} > 
         <Routes>
           <Route path="/" element={<Restaurants />} />
           <Route path="/myReservations" element={<Reservations />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/randomMessage" element={<Message msg="hi" />} />
           <Route path="/restaurants" element={<Elements />} />
           <Route path="/details/:itemId" element={<ElementDetails />} />
         </Routes>
+        </div>
       </Router>
     </Provider>
   );
