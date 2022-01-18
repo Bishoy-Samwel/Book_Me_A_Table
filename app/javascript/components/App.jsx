8c024726298d8f7f3357f35fea9001fae74efa7d
luqmanmusah <musahluqman@gmail.com>
2022-01-18 00:56:04 +0000

    Import the various component into App.jsx

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, {useState} from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -9,21 +9,32 @@ import {
 
 import { Provider } from 'react-redux';
 import configureStore from '../configureStore.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './navbar.jsx';
 import Reservations from './reservations.jsx';
+import Restaurants from './restaurants.jsx';
+import './navbar.css';
+import './App.css';
 
 const store = configureStore();
 
 export default function App() {
+   //menuCollapse state using useState hook
+   const [menuCollapse, setMenuCollapse] = useState(false)
+   //custom function that will change menucollapse state from false to true and true to false
+   const menuIconClick = () => {
+       //condition checking to change state from true to false and vice versa
+       menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
+   };
   return (
     <Provider store={store}>
       <Router>
-        <div>
           
-          <Navbar />
+          <Navbar menuCollapse={menuCollapse} menuIconClick={menuIconClick} />
+          <div className={menuCollapse ? 'ml-2' : 'ml-5'}>
           <Routes>
-            <Route path="/" element={<Reserve />} />
-            <Route path="/randomMessage" element={<Reservations />} />
+            <Route path="/" element={<Restaurants />} />
+            <Route path="/myReservations" element={<Reservations />} />
           </Routes>
         </div>
       </Router>
