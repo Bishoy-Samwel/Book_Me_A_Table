d769f8acf1a4fb56477c10fb1002c9377f93aef7
Bishoy <bishoy.samwuel@gmail.com>
2022-01-19 12:01:55 +0200

    ./bin/rails css:install:bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/extensions */
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import '@hotwired/turbo-rails';
 // import "controllers"
 // import "./components/clock.jsx"
 import './components/App.jsx';
+import * as bootstrap from 'bootstrap';
