02eca8e5a2b1184c96a8d00b6121aabd607c511f
luqmanmusah <musahluqman@gmail.com>
2022-01-20 18:16:10 +0000

    Remove disable linters

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable import/extensions */
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import '@hotwired/turbo-rails';
 // import "controllers"
 // import "./components/clock.jsx"
-import './components/App.jsx';
+import './components/App';
 import * as bootstrap from 'bootstrap';
