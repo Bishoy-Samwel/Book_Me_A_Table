4439b028f05f2b509373cdfb48c7820b56ebfdbd
luqmanmusah <musahluqman@gmail.com>
2022-01-20 06:32:25 +0000

    Fix linter errors

--- app/javascript/components/data.js
+++ app/javascript/components/data.js
@@ -1,39 +1,38 @@
 export const data = [
-    {
-        img: "https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg",
-        desc: "whatever description",
-        title: "this is title",
-        id: 1
-    },
-    {
-        img: "https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg",
-        desc: "whatever description",
-        title: "this is title",
-        id: 2
-    },
-    {
-        img: "https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg",
-        desc: "whatever description",
-        title: "this is title",
-        id: 3
-    },
-    {
-        img: "https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg",
-        desc: "whatever description",
-        title: "this is title",
-        id: 4
-    },
-    {
-        img: "https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg",
-        desc: "whatever description",
-        title: "this is title",
-        id: 5
-    },
-    {
-        img: "https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg",
-        desc: "whatever description",
-        title: "this is title",
-        id: 6
-    },
-  ]
-  
+  {
+    img: 'https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg',
+    desc: 'whatever description',
+    title: 'this is title',
+    id: 1,
+  },
+  {
+    img: 'https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg',
+    desc: 'whatever description',
+    title: 'this is title',
+    id: 2,
+  },
+  {
+    img: 'https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg',
+    desc: 'whatever description',
+    title: 'this is title',
+    id: 3,
+  },
+  {
+    img: 'https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg',
+    desc: 'whatever description',
+    title: 'this is title',
+    id: 4,
+  },
+  {
+    img: 'https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg',
+    desc: 'whatever description',
+    title: 'this is title',
+    id: 5,
+  },
+  {
+    img: 'https://media-cdn.tripadvisor.com/media/photo-s/1a/b8/46/6d/london-stock.jpg',
+    desc: 'whatever description',
+    title: 'this is title',
+    id: 6,
+  },
+];
